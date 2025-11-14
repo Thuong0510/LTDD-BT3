a11e09ed53426c272a13cacf1a49f12f948748cf
Thuong0510 <ngocthuong.th1327@gmail.com>
2025-11-14 14:27:01 +0700

    Signup commit

--- app/src/main/java/com/example/baitap3/LoginActivity.java
+++ app/src/main/java/com/example/baitap3/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.baitap3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +18,11 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        TextView tvRegister = findViewById(R.id.tvRegister);
+        tvRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);  
+        });
     }
+
 }
--- app/src/main/java/com/example/baitap3/SignupActivity.java
+++ app/src/main/java/com/example/baitap3/SignupActivity.java
@@ -1,4 +1,12 @@
 package com.example.baitap3;
 
-public class SignupActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
 }
